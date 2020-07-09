bdbe6456ca5926f1b8f66a77c9eaecd8f1da24c0
Brant Miller <millerbrant@gmail.com>
2020-07-08 20:51:34 -0400

    polling data added to backend js

--- app.js
+++ app.js
@@ -1,74 +1,62 @@
 //Required Libraries
-const express = require('express')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/'
+var url = "mongodb://localhost:27017/";
 
 //Middleware
-const app = express()
-const port = process.env.PORT || '5000'
+const app = express();
+const port = process.env.PORT || "5000";
 
-let allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
 }
-app.use(allowCrossDomain)
+app.use(allowCrossDomain);
+
 
 //endpoints
 app.get('/', (req, res) => {
-  res.send(
-    "<a href='/api/state_data'>State Data</a> <br /> <a href='/api/state_geometry'>State Geo-Geometry</a>",
-  )
+    res.send("<a href='/api/state_data'>State Data</a> <br /> <a href='/api/state_geometry'>State Geo-Geometry</a>");
 })
 
-app.get('/api/state_data', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    dbo
-      .collection('state_data')
-      .find({})
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+app.get("/api/state_data", (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("covid_data_db");
+        dbo.collection("state_data").find({}).toArray(function(err, result) {
+          if (err) throw err;
+          res.send(JSON.stringify(result));
+          db.close();
+        });
+      });
 })
 
-//Find data by date
-app.get('/api/state_data/:date', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    var dateToFind = parseInt(req.params.date)
-    dbo
-      .collection('state_data')
-      .find({ date: dateToFind })
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+app.get("/api/poll_data", (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("covid_data_db");
+        dbo.collection("poll_data").find({}).toArray(function(err, result) {
+          if (err) throw err;
+          res.send(JSON.stringify(result));
+          db.close();
+        });
+      });
 })
 
-app.get('/api/state_geometry', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    dbo
-      .collection('geometry_data')
-      .find({})
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+app.get("/api/state_geometry", (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("covid_data_db");
+      dbo.collection("geometry_data").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+        db.close();
+      });
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`);
 })
