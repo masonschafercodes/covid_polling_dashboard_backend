729f27938edd6b9b5202703806adde648e819bee
Mason <mason.schafer1@gmail.com>
2020-07-10 23:42:53 -0400

    reworked backend to be... well better

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //Required Libraries
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
-
-var url = 'mongodb://localhost:27017/'
+const ObjectId = require('mongodb').ObjectId
+const csv = require('csv-parser')
+const fs = require('fs')
+const fetch = require('node-fetch')
 
 //Middleware
 const app = express()
-const port = process.env.PORT || '5000'
 
 let allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -15,58 +16,101 @@ let allowCrossDomain = function (req, res, next) {
 }
 app.use(allowCrossDomain)
 
+const port = 3000
+
+const mongo_uri = 'mongodb://localhost:27017/'
+const pres_url = 'presidential_polling.csv'
+const usCovidDataPerState = 'https://covidtracking.com/api/v1/states/daily.json'
+
+// create an initial variable
+let dbClient
+// assign the client from MongoClient
+MongoClient.connect(mongo_uri, { useNewUrlParser: true, poolSize: 10 })
+  .then((client) => {
+    db = client.db('covid_data_db')
+    dbClient = client
+    geo_data = db.collection('geometry_data')
+    poll_data = db.collection('poll_data')
+    state_data = db.collection('state_data')
+    app.locals.geo_data = geo_data
+    app.locals.state_data = state_data
+    app.locals.poll_data = poll_data
+    app.listen(port, () => console.info(`REST API running on port ${port}`))
+  })
+  .catch((error) => console.error(error))
+// listen for the signal interruption (ctrl-c)
+process.on('SIGINT', () => {
+  dbClient.close()
+  process.exit()
+})
+
 //endpoints
 app.get('/', (req, res) => {
-  res.send(
-    "<a href='/api/state_data'>State Data</a> <br /> <a href='/api/state_geometry'>State Geo-Geometry</a><br /> <a href='/api/poll_data'>Poll Data</a>",
-  )
+  res
+    .status(200)
+    .json(
+      'Available Routes: /api/state_data, /api/poll_data, /api/state_geometry',
+    )
 })
 
 app.get('/api/state_data', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    dbo
-      .collection('state_data')
-      .find({})
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+  const state_data_col = req.app.locals.state_data
+  state_data_col
+    .find({})
+    .toArray()
+    .then((response) => res.status(200).json(response))
+    .catch((error) => console.error(error))
 })
 
 app.get('/api/poll_data', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    dbo
-      .collection('poll_data')
-      .find({})
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+  const poll_data_col = req.app.locals.poll_data
+  poll_data_col
+    .find({})
+    .toArray()
+    .then((response) => res.status(200).json(response))
+    .catch((error) => console.error(error))
 })
 
 app.get('/api/state_geometry', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    var dbo = db.db('covid_data_db')
-    dbo
-      .collection('geometry_data')
-      .find({})
-      .toArray(function (err, result) {
-        if (err) throw err
-        res.send(JSON.stringify(result))
-        db.close()
-      })
-  })
+  const geo_data_col = req.app.locals.geo_data
+  geo_data_col
+    .find({})
+    .toArray()
+    .then((response) => res.status(200).json(response))
+    .catch((error) => console.error(error))
 })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+app.get('/api/updatealldata', (req, res) => {
+  const state_data_col = req.app.locals.state_data
+  state_data_col.drop(function (err, delOK) {
+    if (err) throw err
+    if (delOK) console.log('Collection deleted')
+  })
+  fetch(usCovidDataPerState)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        if (
+          element.state == 'AS' ||
+          element.state == 'GU' ||
+          element.state == 'MP' ||
+          element.state == 'PR' ||
+          element.state == 'VI'
+        ) {
+          console.log('skipped')
+        } else {
+          var myobj = {
+            state: element.state,
+            deaths: element.death,
+            confirmed_cases: element.positive,
+            date: element.date,
+          }
+          state_data_col.insertOne(myobj, function (err, res) {
+            if (err) throw err
+          })
+        }
+      })
+    })
+
+  res.status(200).json('All Updated')
 })
