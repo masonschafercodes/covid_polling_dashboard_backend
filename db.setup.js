b53198510af5f17442da6c8da16667ab2c32f94c
Mason <mason.schafer1@gmail.com>
2020-07-10 22:31:51 -0400

    noice

--- db.setup.js
+++ db.setup.js
@@ -1,9 +1,12 @@
+//IMPORTS
 const MongoClient = require('mongodb').MongoClient
 const csv = require('csv-parser')
 const fs = require('fs')
 const fetch = require('node-fetch')
+
+//VARIABLES
 var pres_url = 'presidential_polling.csv'
-var url = 'mongodb://localhost:27017/'
+var url = 'mongodb://127.0.0.1:27017/'
 
 //Create a Collection
 const createCollection = (url, colletion_name, db_name) => {
@@ -18,6 +21,7 @@ const createCollection = (url, colletion_name, db_name) => {
   })
 }
 
+//drop Collection function
 const dropCollection = (url, db_name, collection_name) => {
   //Drop Collection
   MongoClient.connect(url, function (err, db) {
@@ -31,92 +35,110 @@ const dropCollection = (url, db_name, collection_name) => {
   })
 }
 
-//(If Collection exists you need to drop the collection first, then run this code after) This will create a new poll_data collection
-// dropCollection(url, 'covid_data_db', 'poll_data')
-
-// createCollection(url, 'covid_data_db', 'geometry_data')
-dropCollection(url, 'covid_data_db', 'state_data')
-// createCollection(url, 'covid_data_db', 'poll_data')
-
-// fs.createReadStream(pres_url)
-//   .pipe(csv())
-//   .on('data', (row) => {
-//     MongoClient.connect(url, function (err, db) {
-//       if (err) throw err
-//       var dbo = db.db('covid_data_db')
-//       var myobj = {
-//         state: row.state,
-//         date: row.modeldate,
-//         candidate: row.candidate_name,
-//         pct: row.pct_trend_adjusted,
-//       }
-//       dbo.collection('poll_data').insertOne(myobj, function (err, res) {
-//         if (err) throw err
-//         console.log('1 document inserted')
-//         db.close()
-//       })
-//     })
-//   })
-//   .on('end', () => {
-//     console.log('CSV file successfully processed')
-//   })
-
-// //(If Collection exists you need to drop the collection first, then run this code after) This will create a new geometry_data collection
-
-// fs.createReadStream('statelatlong.csv')
-//   .pipe(csv())
-//   .on('data', (row) => {
-//     MongoClient.connect(url, function (err, db) {
-//       if (err) throw err
-//       var dbo = db.db('covid_data_db')
-//       if (row.State === 'WA') {
-//         console.log('skipped')
-//       } else {
-//         var myobj = { state: row.State, lat: row.Latitude, long: row.Longitude }
-//         dbo.collection('geometry_data').insertOne(myobj, function (err, res) {
-//           if (err) throw err
-//           console.log('1 document inserted')
-//           db.close()
-//         })
-//       }
-//     })
-//   })
-//   .on('end', () => {
-//     console.log('CSV file successfully processed')
-//   })
-
-// //(Drop existing Collection and Run the bellow code)Get all COVID data per state
-var usCovidDataPerState = 'https://covidtracking.com/api/v1/states/daily.json'
-
-fetch(usCovidDataPerState)
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((element) => {
+//Poll data Function to read in csv and parse out to a collection
+function readPollData() {
+  // dropCollection(url, 'covid_data_db', 'poll_data')
+  fs.createReadStream(pres_url)
+    .pipe(csv())
+    .on('data', (row) => {
       MongoClient.connect(url, function (err, db) {
         if (err) throw err
         var dbo = db.db('covid_data_db')
-        //AS, GU, MP, PR, VI
-        if (
-          element.state == 'AS' ||
-          element.state == 'GU' ||
-          element.state == 'MP' ||
-          element.state == 'PR' ||
-          element.state == 'VI'
-        ) {
+        var myobj = {
+          state: row.state,
+          date: row.modeldate,
+          candidate: row.candidate_name,
+          pct: row.pct_trend_adjusted,
+        }
+        dbo.collection('poll_data').insertOne(myobj, function (err, res) {
+          if (err) throw err
+          console.log('1 document inserted')
+        })
+        db.close()
+      })
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed')
+    })
+}
+
+//Geo data Function to read in csv and parse out to a collection
+function readGeoData() {
+  // dropCollection(url, 'covid_data_db', 'geometry_data')
+  fs.createReadStream('statelatlong.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      MongoClient.connect(url, function (err, db) {
+        if (err) throw err
+        var dbo = db.db('covid_data_db')
+        if (row.State === 'WA') {
           console.log('skipped')
         } else {
           var myobj = {
-            state: element.state,
-            deaths: element.death,
-            confirmed_cases: element.positive,
-            date: element.date,
+            state: row.State,
+            lat: row.Latitude,
+            long: row.Longitude,
           }
-          dbo.collection('state_data').insertOne(myobj, function (err, res) {
+          dbo.collection('geometry_data').insertOne(myobj, function (err, res) {
             if (err) throw err
             console.log('1 document inserted')
-            db.close()
           })
         }
+        db.close()
       })
     })
-  })
+    .on('end', () => {
+      console.log('CSV file successfully processed')
+    })
+}
+
+//Fetch api data, parse out and add to collection
+function getStateData() {
+  // dropCollection(url, 'covid_data_db', 'state_data')
+  var usCovidDataPerState = 'https://covidtracking.com/api/v1/states/daily.json'
+
+  fetch(usCovidDataPerState)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        MongoClient.connect(url, function (err, db) {
+          if (err) throw err
+          var dbo = db.db('covid_data_db')
+          //AS, GU, MP, PR, VI
+          if (
+            element.state == 'AS' ||
+            element.state == 'GU' ||
+            element.state == 'MP' ||
+            element.state == 'PR' ||
+            element.state == 'VI'
+          ) {
+            console.log('skipped')
+          } else {
+            var myobj = {
+              state: element.state,
+              deaths: element.death,
+              confirmed_cases: element.positive,
+              date: element.date,
+            }
+            dbo.collection('state_data').insertOne(myobj, function (err, res) {
+              if (err) throw err
+              console.log('1 document inserted')
+            })
+          }
+          db.close()
+        })
+      })
+    })
+}
+
+//Calling all update Database functions
+function updateDatabase() {
+  getStateData()
+  readGeoData()
+  readPollData()
+}
+
+updateDatabase()
+
+//Runs script once every day
+// setInterval(updateDatabase, 86400000)
